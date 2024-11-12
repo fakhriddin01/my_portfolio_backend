6fe5f65b5299e6b86006ea4c81531ba8fe4b48f2
Fakhriddin Abduraimov <106900498+fakhriddin01@users.noreply.github.com>
2024-11-12 22:59:24 +0500

    Update experience.controller.ts

--- src/experience/experience.controller.ts
+++ src/experience/experience.controller.ts
@@ -5,11 +5,12 @@ import { UpdateExperienceDto } from './dto/update-experience.dto';
 import { IsAdminGuard } from '../guards/isAdmin.guard';
 
 
-@UseGuards(IsAdminGuard)
+
 @Controller('experience')
 export class ExperienceController {
   constructor(private readonly experienceService: ExperienceService) {}
 
+  @UseGuards(IsAdminGuard)
   @Post()
   create(@Body() createExperienceDto: CreateExperienceDto) {
     return this.experienceService.create(createExperienceDto);
@@ -25,11 +26,13 @@ export class ExperienceController {
     return this.experienceService.findOne(+id);
   }
 
+  @UseGuards(IsAdminGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateExperienceDto: UpdateExperienceDto) {
     return this.experienceService.update(+id, updateExperienceDto);
   }
 
+  @UseGuards(IsAdminGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.experienceService.remove(+id);
