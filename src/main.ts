08dbb11d20d7e1082ab45ed8c98d24de4a22a83b
Fakhriddin Abduraimov <106900498+fakhriddin01@users.noreply.github.com>
2024-11-12 11:47:06 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,33 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Logger } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { urlencoded, json } from 'express';
 
 async function start() {
-  try {
-    const app = await NestFactory.create(AppModule, {cors:true});
-    const PORT = process.env.PORT || 3000;
-    
-    app.setGlobalPrefix('api');
-    app.use(json({ limit: '50mb' }));
-    app.use(urlencoded({ extended: true, limit: '50mb' }));
-    await app.listen(PORT, ()=>{
-      console.log(`server running on port: ${PORT}`);
-      
-    });
-  } catch (error) {
-      console.log(error);
-  } 
+  const logger = new Logger('HTTP'); // Logger for HTTP requests
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const PORT = process.env.PORT || 3000;
   
+  app.setGlobalPrefix('api');
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  // Middleware to log each request
+  app.use((req, res, next) => {
+    const { method, originalUrl } = req;
+    const start = Date.now();
+
+    res.on('finish', () => {
+      const delay = Date.now() - start;
+      const { statusCode } = res;
+      logger.log(`${method} ${originalUrl} ${statusCode} - ${delay}ms`);
+    });
+
+    next();
+  });
+
+  await app.listen(PORT, () => {
+    logger.log(`Server running on port: ${PORT}`);
+  });
 }
+
 start();
