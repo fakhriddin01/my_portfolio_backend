ce641271827e3086e0349d3ae723c37d4547e8ba
Fakhriddin Abduraimov <106900498+fakhriddin01@users.noreply.github.com>
2024-11-12 21:01:41 +0500

    Update files.service.ts

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -9,7 +9,7 @@ export class FilesService {
     async createFile(file: any): Promise<string> {
         try {
             const fileName = uuid.v4() + file.originalname;
-            const filePath = path.resolve(__dirname,'..', 'static');
+            const filePath = path.resolve(__dirname,'..', '..', 'static');
             
             if(!fs.existsSync(filePath)) {
                 fs.mkdirSync(filePath, { recursive: true});
@@ -28,7 +28,7 @@ export class FilesService {
 
         async removeFile(fileName: string){
             try {
-                const filePath = path.resolve(__dirname, '..', 'static');
+                const filePath = path.resolve(__dirname, '..', '..', 'static');
                 fs.rmSync(path.join(filePath, fileName));
                 console.log("file deleted");
                 return fileName;
@@ -60,7 +60,7 @@ export class FilesService {
                 const extension = contentType.split('/')[1];
                 const filename = `${uuid.v4()}.${extension}`; // Unique filename using UUIDv4
                 
-                const filePath = path.resolve(__dirname,'..', 'static');
+                const filePath = path.resolve(__dirname,'..', '..', 'static');
                 if(!fs.existsSync(filePath)) {
                     fs.mkdirSync(filePath, { recursive: true});
                 }
